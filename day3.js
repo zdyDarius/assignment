27aeb7460e9f3d0d3617cc9648c0291183210c18
deyang.zhou <deyang.zhou@17zuoye.com>
2022-03-30 18:16:36 +0800

    fix

--- day3.js
+++ day3.js
@@ -5,52 +5,89 @@
     2.法题：https://leetcode-cn.com/problems/design-a-stack-with-increment-operation/
     🍊志佳老师🍊
  */
+function allSettled(promises) {
+    // your code here
+    return new Promise((resolve) => {
+        promises = Array.isArray(promises) ? promises : [];
+        let length = promises.length;
+        let resArgs = new Array(length);
+        if (length === 0) {
+            resolve(resArgs)
+        }
+        const watchResArgs = () => {
+            length -= 1;
+            if (length === 0) resolve(resArgs)
+        }
+        for (let i = 0; i < promises.length; i++) {
+            if (promises[i] instanceof Promise) {
+                promises[i].then(value => {
+                    console.log(value)
+                    resArgs[i] = { status: 'fulfilled', value }
+                    watchResArgs()
+                }, reason => {
+                    console.log(reason)
+                    resArgs[i] = { status: 'rejected',  reason }
+                    watchResArgs()
+                })
+            } else {
+                resArgs[i] = { status: 'fulfilled', value: promises[i] }
+                watchResArgs()
+            }
+        }
+    })
+};
 
-    /**
- * @param {number} maxSize
- */
-var CustomStack = function(maxSize) {
-    this.maxSize=maxSize;
+const promise1 = Promise.resolve(3);
+const promise2 = new Promise((resolve, reject) => setTimeout(reject, 100, 'foo'));
+const promises = [promise1, promise2,2];
+
+allSettled([1,2,3, Promise.reject('error')]).then((results) => console.log(results));
+
+/**
+* @param {number} maxSize
+*/
+var CustomStack = function (maxSize) {
+    this.maxSize = maxSize;
     this.arr = new Array()
-  };
-  
-  /** 
-   * @param {number} x
-   * @return {void}
-   */
-  CustomStack.prototype.push = function(x) {
-      if ( this.arr.length < this.maxSize) {
-         this.arr[this.arr.length] = x
-      }
-  };
-  
-  /**
-   * @return {number}
-   */
-  CustomStack.prototype.pop = function() {
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+CustomStack.prototype.push = function (x) {
+    if (this.arr.length < this.maxSize) {
+        this.arr[this.arr.length] = x
+    }
+};
+
+/**
+ * @return {number}
+ */
+CustomStack.prototype.pop = function () {
     if (this.arr.length) {
-        return ( this.arr ).splice( this.arr.length - 1 , 1 )[0]
-      }
-      return -1
-  };
-  
-  /** 
-   * @param {number} k 
-   * @param {number} val
-   * @return {void}
-   */
-  CustomStack.prototype.increment = function(k, val) {
+        return (this.arr).splice(this.arr.length - 1, 1)[0]
+    }
+    return -1
+};
+
+/** 
+ * @param {number} k 
+ * @param {number} val
+ * @return {void}
+ */
+CustomStack.prototype.increment = function (k, val) {
     const length = Math.min(this.arr.length, k)
     for (let i = 0; i < length; i++) {
-      this.arr[i] += val
+        this.arr[i] += val
     }
-  };
+};
 
-  
-  /**
-   * Your CustomStack object will be instantiated and called as such:
-   * var obj = new CustomStack(maxSize)
-   * obj.push(x)
-   * var param_2 = obj.pop()
-   * obj.increment(k,val)
-   */
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * var obj = new CustomStack(maxSize)
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * obj.increment(k,val)
+ */
